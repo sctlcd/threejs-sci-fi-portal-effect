f5b141c004594780e62a255a33f4bea22e6533a4
lucinda <lucinda.souchet@gmail.com>
2023-03-08 17:02:56 +0100

    add scene, camera, renderer and render function

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 import './assets/style/style.css'
 import * as THREE from 'three';
 
+/***************************************************** Scene */
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+/***************************************************** Camera */
+
+const camera = new THREE.PerspectiveCamera(
+  80, // fov = Field Of View
+  window.innerWidth / window.innerHeight, // aspect ratio = width / height
+  1, // near clipping plane
+  10000 // far clipping plane
+);
+camera.position.set(0, 0, 1000);
+scene.add(camera);
+
+/***************************************************** Renderer */
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setClearColor(0x000000, 1);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Add renderer into HTML as a canvas element
+document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+/***************************************************** Render */
 
-function animate() {
-  requestAnimationFrame( animate );
+function render() {
+  // rerender every time the page refreshes (pause when on another tab)
+  requestAnimationFrame(render);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
-animate();
+render();
